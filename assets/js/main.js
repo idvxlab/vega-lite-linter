1f4a9012446aac1073508f0a189593b868f88d8e
soundquiet <625941917@qq.com>
2021-04-29 00:07:57 +0800

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,14 +6,13 @@ $(document).ready(function() {
     var elements = $('.sticky');
     Stickyfill.add(elements);
 
-
     /* Activate scrollspy menu */
     $('body').scrollspy({target: '#doc-menu', offset: 100});
     
     /* Smooth scrolling */
 	$('a.scrollto').on('click', function(e){
         //store hash
-        var target = this.hash;    
+        var target = this.hash;   
         e.preventDefault();
 		$('body').scrollTo(target, 800, {offset: 0, 'axis':'y'});
 		
